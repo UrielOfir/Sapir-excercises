606acbbb94dd9240c48e475db1fb60551b94e6cf
UrielOfir <urielofir@gmail.com>
2019-05-20 16:02:28 +0300

    almost done

--- js.js
+++ js.js
@@ -2,8 +2,7 @@ var i = 0,images = ["images/index1.jpeg",
                     "images/index2.jpeg",
                     "images/index3.jpeg"];
 
-         function mySlide(param)
-         {
+         function mySlide(param){
              if(param === 'next')
              {
                  i++;
@@ -15,6 +14,15 @@ var i = 0,images = ["images/index1.jpeg",
 
              document.getElementById('slide').src = images[i];
          }
+
+         var myText = document.getElementById("comments");
+         var wordCount = document.getElementById("wordCount");
+
+         myText.addEventListener("keyup",function(){
+ 	         var characters = myText.value.split('');
+           wordCount.innerText = 200-characters.length;
+         });
+
          function validateForm(){
          var name = document.forms["myForm"]["FullName"];
              var email = document.forms["myForm"]["EMail"];
@@ -24,9 +32,21 @@ var i = 0,images = ["images/index1.jpeg",
 
              if (!FullName.value.match(/^[A-Za-z]+$/))
        {
-           window.alert("Please enter your name.");
+           window.alert("Please enter your name correctly.");
            name.focus();
            return false;
        }
 
-          }
+       if(phone.value.length<9||!phone.value.match(/^[0-9]+$/)) {
+            window.alert("Please enter your phone correctly .");
+            phone.focus();
+            return false;
+        }
+
+        if(gestsNum.value<=0||!phone.value.match(/^[0-9]+$/))
+        {
+             window.alert("How many guests are coming?");
+             gestsNum.focus();
+             return false;
+         }
+       }
